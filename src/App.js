d6c576400996f74f7e3b1dbd1cd04008cc40d8c5
Suyash00 <suyashhandy@gmail.com>
2024-01-06 00:03:53 +0530

    dictionary deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [definition, setDefinition] = useState("");
+  const [showDefinition, setShowDefinition] = useState(false);
+  const dictionary = [
+    {
+      word: "React",
+      meaning: "A JavaScript library for building user interfaces.",
+    },
+    { word: "Component", meaning: "A reusable building block in React." },
+    { word: "State", meaning: "An object that stores data for a component." },
+  ];
+
+  const handleChange = (e) => {
+    setShowDefinition(false);
+    const word = e.target.value;
+    setInputText(word);
+  };
+  const handleSearch = () => {
+    const item = dictionary.find(
+      (item) => item.word.toLowerCase() === inputText.toLowerCase()
+    );
+    if (item) {
+      setDefinition(item.meaning);
+    } else {
+      setDefinition(null);
+    }
+    setShowDefinition(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Dictionary App</h1>
+      <input
+        type="text"
+        placeholder="Search for a word..."
+        value={inputText}
+        onChange={handleChange}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <h4>Definition:</h4>
+      {showDefinition && (
+        <div>
+          {definition ? definition : "Word not found in the dictionary."}
+        </div>
+      )}
     </div>
   );
 }
